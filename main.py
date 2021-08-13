39a2bda39abe399fabdfe878d99cd1f8e9813cef
cutebear <72639017+cutebear0123@users.noreply.github.com>
2021-08-13 10:15:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import os,asyncio
 import discord
 
-try:
-	import requests
-except:
-	os.system('pip install requests')
-	import requests
+import requests
 from io import BytesIO
-from discord.ext import commands
-try:
-    from PIL import Image
-except:
-    os.system('pip install pillow')
-    from PIL import Image
+from PIL import Image
 
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='oil',intents=intents)
+bot = discord.Client()
 
 @bot.event
 async def on_ready():
@@ -36,5 +27,5 @@ async def on_member_join(member):
                 gif.append(image)
         gif[0].save('welcome.gif',save_all=True,optimize=False, append_images=gif[1:], loop=0)
         channel = discord.utils.get(member.guild.channels, name='welcome')
-        await channel.send(file=discord.File('welcome.gif'))
-bot.run('token')
+        await channel.send(file=discord.File('./welcome.gif'))
+bot.run(token)
